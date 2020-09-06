45b19e4b128fd447b57df812263d7bfd7f27bcac
Joris Meys <Joris.Meys@UGent.be>
2020-09-06 13:44:57 +0200

    typo

--- checkPackages.R
+++ checkPackages.R
@@ -1,7 +1,6 @@
 # Checks and loads the necessary packages
 #----------------------------------------
 
-Check whether the necessary packages are installed.
 if(!suppressWarnings(library("doesntexist", logical.return = TRUE,
             quietly = TRUE))){
     stop("Package needs installing.", call. = FALSE)
